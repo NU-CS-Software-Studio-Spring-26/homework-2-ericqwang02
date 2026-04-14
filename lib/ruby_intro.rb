5244efb188a01bc22635544abc8924e0e64d1082
ericwang100 <ericwang2025@u.northwestern.edu>
2026-04-13 23:53:48 -0500

    hw2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,55 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr.first if arr.length == 1
+
+  arr.max(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.length < 2
+
+  seen = {}
+  arr.each do |value|
+    return true if seen[n - value]
+
+    seen[value] = true
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.match?(/\A(?i:[b-df-hj-np-tv-z])/)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s.match?(/\A0\z|\A[01]*00\z/)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', price)
+  end
 end
